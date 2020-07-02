54774a296a52754e605ff9c5e411296d30d8e958
Nikos Patikas <nik.patik@gmail.com>
2020-07-02 19:14:16 +0100

    initial commit

---
+++ init.el
@@ -0,0 +1,131 @@
+
+
+;; Supress starting message
+(setq inhibit-startup-message t)
+
+(setq w3m-default-display-inline-images t)
+
+
+;; dvorak swap C-x with C-u
+(define-key key-translation-map [?\C-x] [?\C-u])
+(define-key key-translation-map [?\C-u] [?\C-x])
+
+;; dvorak help binding remap to map 
+(global-set-key (kbd "C-x C-h") help-map)
+(global-set-key (kbd "C-h") 'previous-line)
+
+;; Hide toolbar, menu and scroll-bar
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; highlight point line
+(global-hl-line-mode t)
+;; show line numbers
+(line-number-mode t)
+
+
+
+;; load package
+(require 'package)
+(package-initialize)
+
+;; to see package package-archive contents execute C-h v package-archives
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+
+;; remember recently opened buffers
+(recentf-mode 1)
+(setq recentf-max-menu-items 50) ;; last n
+(setq recentf-max-saved-items 50)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+
+;; match parentheses
+(show-paren-mode 1)
+
+;; make my life easier with IDO
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+;; C-c C-p to check available commands
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+
+
+;; theme settings
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes
+   (quote
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "f3ab34b145c3b2a0f3a570ddff8fabb92dafc7679ac19444c31058ac305275e1" default)))
+ '(ein:output-area-inlined-images t)
+ '(hl-todo-keyword-faces
+   (quote
+    (("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#3a81c3")
+     ("OKAY" . "#3a81c3")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#42ae2c")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX+" . "#dc752f")
+     ("\\?\\?\\?+" . "#dc752f"))))
+ '(package-selected-packages
+   (quote
+    (projectile treemacs use-package w3m ein spacemacs-theme monokai-theme conda bbdb)))
+ '(pdf-view-midnight-colors (quote ("#655370" . "#fbf8ef"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+
+
+
+;; use imagemagick, if available
+(when (fboundp 'imagemagick-register-types)
+  (imagemagick-register-types))
+
+
+
+(add-to-list 'load-path "~/.emacs.d/")
+(load "mu4e-config")
+
+
+(use-package projectile
+  :ensure t
+  :config
+  (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+  (projectile-mode +1)
+  )
+(use-package treemacs
+  :ensure t
+  :bind
+  (:map global-map
+	([f8] . treemacs)
+	("C-<f8>" .  treemacs-select-window)
+	)
+  :config
+  (setq treemacs-is-never-other-window t))
+
+
+
---
+++ mu4e-config.el
@@ -0,0 +1,141 @@
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+
+(require 'mu4e)
+
+
+(global-set-key  (kbd "C-c m") 'mu4e)
+  
+
+;; Dummy to be overriden by context
+(setq
+ send-mail-function 'smtpmail-send-it
+ message-send-mail-function 'smtpmail-send-it)
+
+
+(require 'cl)
+(defun my-make-mu4e-context (name address server port ssltype signature)
+  "Return a mu4e context named NAME with :match-func matching
+  its ADDRESS in From or CC fields of the parent message. The
+  context's `user-mail-address' is set to ADDRESS and its
+  `mu4e-compose-signature' to SIGNATURE."
+  (lexical-let ((addr-lex address))
+    (make-mu4e-context :name name
+		       :vars `((user-mail-address . ,address)
+			       ( user-full-name       . "Nikos Patikas" )
+			       ( mu4e-compose-signature . ,signature)
+			       ( smtpmail-smtp-server   . ,server)
+			       ( smtpmail-smtp-service  . ,port)
+			       ( smtpmail-stream-type   . ,ssltype)
+			       )
+		       :match-func
+		       (lambda (msg)
+			 (when msg
+			   (or (mu4e-message-contact-field-matches msg :to addr-lex)
+			       (mu4e-message-contact-field-matches msg :cc addr-lex)))))))
+
+(setq professional-signature "--------------------------------\n Nikolaos Patikas\n PhD student/Bioinformatician \n Metzakopian lab\n UK Dementia Research Institute\n University of Cambridge\n\n Web: https://ukdri.ac.uk")
+;; Add my contexts
+
+(setq mu4e-contexts
+      `( , (my-make-mu4e-context
+	    "medschl"
+	    "np504@medschl.cam.ac.uk"
+	    "127.0.0.1"
+	    1025
+	    'plain
+	    professional-signature)
+	   
+           , (my-make-mu4e-context
+	      "sanger"
+	      "np13@sanger.ac.uk"
+	      "127.0.0.1"
+	      1026
+	      'plain
+	      professional-signature)
+	   
+	   , (my-make-mu4e-context
+	      "cambridge"
+	      "np504@cam.ac.uk"
+	      "127.0.0.1"
+	      1027
+	      'plain
+	      professional-signature)
+	   ))
+
+
+
+
+(setq
+ mu4e-context-policy 'pick-first ;; start with the first (default) context; 
+ mu4e-compose-context-policy nil ;; compose with the current context if no context matches;
+)
+
+
+
+
+;; Make html messages view properly in a dark background
+(setq shr-color-visible-luminance-min 80)
+
+
+(setq
+ message-kill-buffer-on-exit t
+ mu4e-view-show-images t
+ mu4e-image-max-width 1920
+ ;;mu4e-html2text-command "w3m -dump -T text/html"
+ ;;w3m-command "/usr/bin/w3m"
+ mu4e-view-prefer-html t
+ )
+
+
+
+
+;; (defun mu4e-action-view-in-w3m ()
+;;   "View the body of the message in emacs w3m."
+;;   (interactive)
+;;   (w3m-browse-url (concat "file://"
+;;               (mu4e-write-body-to-html (mu4e-message-at-point t)))))
+
+
+(setq
+       mu4e-get-mail-command "/usr/bin/mbsync -aV"   ;; or fetchmail, or ...
+       mu4e-update-interval nil)
+
+
+(defun mu4e-action-view-in-w3m (msg)
+  "View the body of the message in emacs w3m."
+  (interactive)
+  (w3m-browse-url (concat "file://"
+			  (mu4e~write-body-to-html msg))))
+
+
+(defun mu4e-action-view-in-w3m (msg)
+  "View the body of MSG in a w3m."
+  (w3m-browse-url (concat "file://"
+                      (mu4e~write-body-to-html msg))))
+
+
+(add-to-list 'mu4e-view-actions '("ViewInBrowser" . mu4e-action-view-in-browser) t)
+(add-to-list 'mu4e-view-actions '("W3M-view" . mu4e-action-view-in-w3m) t)
+
+
+(setq
+       mu4e-sent-folder   "/folders/sent"       ;; folder for sent messages
+       mu4e-drafts-folder "/folders/drafts"     ;; unfinished messages
+       mu4e-trash-folder  "/folders/trash"      ;; trashed messages
+       mu4e-refile-folder "/folders/archive")
+
+
+
+
+;;(setq mu4e-html2text-command "w3m -dump -T text/html")
+
+
+(add-hook 'mu4e-view-mode-hook
+  (lambda()
+    ;; try to emulate some of the eww key-bindings
+    (local-set-key (kbd "<tab>") 'shr-next-link)
+    (local-set-key (kbd "<backtab>") 'shr-previous-link)
+    ))
+
+
+
